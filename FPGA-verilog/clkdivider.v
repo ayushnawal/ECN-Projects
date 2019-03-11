195ebdfbcbb00a0b3fdf2319a56a67e8c5685dd1
Ayush Nawal <36100944+ayushnawal@users.noreply.github.com>
2019-03-12 01:51:14 +0530

    source files

---
+++ FPGA-verilog/clkdivider.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.02.2019 15:22:43
+// Design Name: 
+// Module Name: clkdivider
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clkdivider(
+
+    );
+endmodule
